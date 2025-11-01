66adb06df1d7f8b974ecd0ed2fdb3f40fcc53d6e
Mingy <mingy.lin@ttfb-lab.com>
2025-11-01 22:40:53 +0800

    增加正式 config

--- getConfig.js
+++ getConfig.js
@@ -1,3 +1,4 @@
+// dev
 function getConfig() {
 	return {
 		allowedCalendarId: '0a437281002a546d7e17233cefa484100bd212d88da1c792e9f162bdf1be23e3@group.calendar.google.com',
@@ -6,3 +7,16 @@ function getConfig() {
 		stockReportDestId: '1EsDKBw0malhT8o0s_OF28QMnNfScTerF-FK-9W36GOE'
 	};
 }
+
+
+// prod
+/*
+function getConfig() {
+	return {
+		allowedCalendarId: 'fcdbf47498c0e3be09237ac9b5df1bb8dad2f714df9f191664661113fa41fded@group.calendar.google.com',
+        mainSheetId: '1D2F3xYzDoyv4IiB70WC8-WsREX9jk53cOMy76SoHPjQ',		
+		teacherReportDestId: '1p6s_xpBGgyMT2daz-ha35ALWRf-W82wlT9U3wW4iX_4',
+		stockReportDestId: '1p6s_xpBGgyMT2daz-ha35ALWRf-W82wlT9U3wW4iX_4'
+	};
+}
+*/
